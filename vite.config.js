40d67aef4de57c412ec85ee61eb0f24091adfad7
vjkarthi2905 <166390950+vjkarthi2905@users.noreply.github.com>
2024-12-12 01:44:45 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import path from 'path'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './'),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: 'index.html'
+      }
+    }
+  },
+  server: {
+    port: 3000,
+    open: true
+  }
+})
